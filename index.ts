5cf0d728546e47de4f0b615e9c070c42b112a252
mikaeel khalid <mikaeelkhalid@icloud.com>
2023-12-15 08:01:35 +0100

    deploy a pre-existing app

--- index.ts
+++ index.ts
@@ -4,3 +4,12 @@ import * as gcp from '@pulumi/gcp';
 // location to deploy cloud run service
 const location = gcp.config.region || 'us-central1';
 
+// deploy a pre-existing Hello Cloud Run container
+const helloService = new gcp.cloudrun.Service('hello', {
+  location,
+  template: {
+    spec: {
+      containers: [{ image: 'gcr.io/cloudrun/hello' }],
+    },
+  },
+});
